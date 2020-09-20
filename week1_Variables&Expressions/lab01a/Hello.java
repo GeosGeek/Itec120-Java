27785cf1a6bbbc8a2073d7fb304d5c10e8a66276
GeosGeek <47164801+GeosGeek@users.noreply.github.com>
2020-09-20 19:01:44 -0400

    Adding code for week 1
    Variables and Expressions

---
+++ week1_Variables&Expressions/lab01a/Hello.java
@@ -0,0 +1,14 @@
+\**
+ * Hello.java
+ * @author Matt Crichton
+ * @version AUG-27-2018
+ *
+ **\
+ public class Hello
+ {
+	public static void main (String[] args)
+	{
+		System.out.println("\nHello World!\n")
+	}
+ }
+ 
